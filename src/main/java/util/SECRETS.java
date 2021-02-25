44f0bb054604c14dd2be8b0945194b8625f76ac5
Joel Hoffmann <joho.hoffmann@gmx.de>
2021-02-25 20:06:52 +0100

    SECRETS

--- src/main/java/util/SECRETS.java
+++ src/main/java/util/SECRETS.java
@@ -2,8 +2,8 @@ package util;
 
 public class SECRETS {
 
-    public static String Token = "";
-    public static final String clientId = "";
-    public static final String clientSecret = "";
+    public static String Token = "Nzc4NzA2NzIxMTQ1MjI1MjQ2.X7V5JQ.Doyban5Hi4ENsmit2EQP7KrkFS8";
+    public static final String clientId = "42e28ac68af041068f3bedc2c7cc4395";
+    public static final String clientSecret = "9d278282401b4f64b1f66c09733e524c";
 
 }
