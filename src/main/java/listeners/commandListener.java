e27de3da9bef98f8f9acc132711fe12dad1da6d5
NXZAS8CA <simon.krieger@mailbox.org>
2021-04-08 21:25:17 +0200

    fixed typo in commandListener.java

--- src/main/java/listeners/commandListener.java
+++ src/main/java/listeners/commandListener.java
@@ -77,35 +77,35 @@ public class commandListener extends ListenerAdapter {
         //Musik
         if (event.getReactionEmote().getEmoji().contains(STATIC.EmoteforSkip)) {
             event.getReaction().removeReaction(event.getUser()).queue();
-            cmdMusic musicControler = new cmdMusic();
-            musicControler.skip();
+            cmdMusic musicController = new cmdMusic();
+            musicController.skip();
         }
         if (event.getReactionEmote().getEmoji().contains(STATIC.EmoteforStop)) {
             event.getReaction().removeReaction(event.getUser()).queue();
-            cmdMusic musicControler = new cmdMusic();
-            musicControler.stop();
+            cmdMusic musicController = new cmdMusic();
+            musicController.stop();
 
         }
         if (event.getReactionEmote().getEmoji().contains(STATIC.EmoteforShuffle)) {
             event.getReaction().removeReaction(event.getUser()).queue();
-            cmdMusic musicControler = new cmdMusic();
-            musicControler.shuffle();
+            cmdMusic musicController = new cmdMusic();
+            musicController.shuffle();
 
         }
         if(event.getReactionEmote().getEmoji().contains(STATIC.EmoteforPause)){
             event.getReaction().removeReaction(event.getUser()).queue();
-            cmdMusic musicControler = new cmdMusic();
-            musicControler.pause();
+            cmdMusic musicController = new cmdMusic();
+            musicController.pause();
         }
         if (event.getReactionEmote().getEmoji().contains(STATIC.Emoteforlower)) {
             event.getReaction().removeReaction(event.getUser()).queue();
-            cmdMusic musicControler = new cmdMusic();
-            musicControler.vol_lower();
+            cmdMusic musicController = new cmdMusic();
+            musicController.vol_lower();
         }
         if (event.getReactionEmote().getEmoji().contains(STATIC.Emoteforhigher)) {
             event.getReaction().removeReaction(event.getUser()).queue();
-            cmdMusic musicControler = new cmdMusic();
-            musicControler.vol_higher();
+            cmdMusic musicController = new cmdMusic();
+            musicController.vol_higher();
         }
     }
 }
